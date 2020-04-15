330477b835df876e69e8bbf0c389084294106c52
Smaranjit Ghose <46641503+smaranjitghose@users.noreply.github.com>
2020-04-15 19:41:40 +0530

    Added Tree Fractal Script

---
+++ tree_fractal.py
@@ -0,0 +1,26 @@
+import turtle  # importing our graphical library
+import math #importing our mathematical function library
+turtle.speed(10)  # Set speed of drawing
+turtle.hideturtle()  # Disable default turtle object
+s = turtle.Screen()  # Create a graphic window
+s.bgcolor("black")  # Set background color
+turtle.pencolor("red")  # Set drawing color
+s.title("Tree Fractal By Smaranjit Ghose")  # Set Title
+turtle.left(90)
+
+
+def tree(d,angle):
+  '''
+  The function to draw the Tree Fractal
+  '''
+  if d > 5:
+    turtle.forward(d)
+    turtle.right(angle)
+    tree(d/math.sqrt(2),angle)
+    turtle.left(2*angle)
+    tree(d/math.sqrt(2),angle)
+    turtle.right(angle)
+    turtle.backward(d)
+
+
+tree(100,20)
